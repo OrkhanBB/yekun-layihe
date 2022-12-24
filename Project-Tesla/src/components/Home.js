732ad11d25ab720d1be437e4297a62fdf222df29
OrkhanBB <108542593+OrkhanBB@users.noreply.github.com>
2022-12-24 13:43:37 +0400

    Add files via upload

---
+++ Project-Tesla/src/components/Home.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+function Home() {
+  return (
+    <Container>
+        <Section
+        title="Model 3"
+        description="Schedule a Test Drive"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        /> 
+          <Section
+        title="Model Y"
+        description="Schedule a Test Drive"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        />
+        <Section
+        title="Model S"
+        description="Schedule a Test Drive"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        />
+      
+       
+        <Section
+        title="Model X"
+        description="Schedule a Test Drive"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        />
+        <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn More"
+        />
+        <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+        rightBtnText=""
+        />
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+    height:100vh;
+
+`
